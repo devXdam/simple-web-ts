ed68030f2af6e08194cb6a6eb3fde9f7bc416ba0
devxdam <desarrollo_multiplataforma@hotmail.com>
2024-09-15 23:28:15 +0200

    v1.0 actualizadas las rutas de los archivos json ya se pudo desplegar en netlify y vercel

--- src/components/navbar/NavbarComponent.ts
+++ src/components/navbar/NavbarComponent.ts
@@ -2,7 +2,7 @@ import { SidebarComponent } from "../sidebar/SidebarComponent";
 import "./styles/navbar.css";
 export const NavbarComponent = () => {
     const navbar = new SidebarComponent();
-    navbar.setAttribute("data-url", "src/data/pages.json");
+    navbar.setAttribute("data-url", "/data/pages.json");
     navbar.setAttribute("data-ul-class", "navbar");
     return navbar;
 }
