489c83fa0899bf12ca6f96f0761af8d68c3b9141
devxdam <desarrollo_multiplataforma@hotmail.com>
2024-09-16 00:11:07 +0200

    v1.02 se actualizan los estilos de las secciones del home

--- src/components/section/SectionComponent.ts
+++ src/components/section/SectionComponent.ts
@@ -1,49 +1,56 @@
 import "./styles/section.css";
+
 export class SectionComponent extends HTMLElement {
     constructor() {
-      super();
+        super();
     }
-  
+
     connectedCallback() {
-      const url = this.getAttribute('data-url');
-      this.fetchData(url as string);
+        const url = this.getAttribute('data-url');
+        if (url) {
+            this.fetchData(url);
+        } else {
+            console.error('No URL provided for fetching data.');
+        }
     }
-  
-    async fetchData(url: string) {
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        this.render(data.sections);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+
+    async fetchData(url: string) { // Especificar el tipo de 'url' como string
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            this.render(data.sections);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
     }
-  
+
     render(sections: any[]) {
-      this.innerHTML = sections.map(section => `
-        <section class="${section.class}" id="${section.id}">
-          <div class="col-left">
-            <header class="title-section"><h3>${section.title}</h3></header>
-            <article>
-              <p>${section.content}</p>
-            </article>
-            <button-wc>+ info</button-wc>
-          </div>
-          <div class="col-right">
-            <picture>
-              <img
-                width="300px"
-                height="300px"
-                src="${section.imgSrc}"
-                alt="${section.imgAlt}"
-                loading="lazy"
-              />
-            </picture>
-          </div>
-        </section>
-      `).join('');
+        this.innerHTML = sections.map(section => `
+            <section class="${section.class}" id="${section.id}">
+                <div class="text-container">
+                    <header class="title-section">
+                        <h3>${section.title}</h3>
+                    </header>
+                    <article>
+                        <p>${section.content}</p>
+                    </article>
+                    <button-wc>+ info</button-wc>
+                </div>
+                <div class="image-container">
+                    <picture>
+                        <img
+                            src="${section.imgSrc}"
+                            alt="${section.imgAlt}"
+                            loading="lazy"
+                        />
+                    </picture>
+                </div>
+            </section>
+        `).join('');
     }
-  }
-  
-  customElements.define('section-component', SectionComponent);
-  
+}
+
+customElements.define('section-component', SectionComponent);
