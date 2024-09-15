90a0476619f3acc210102067f1d5039cbc361283
devxdam <desarrollo_multiplataforma@hotmail.com>
2024-09-15 23:13:08 +0200

    v0.03 se cambia la ruta de la carpeta datay actualiza la ruta de los estilos en el index

--- src/views/pages/home.ts
+++ src/views/pages/home.ts
@@ -1,7 +1,7 @@
 import { SectionComponent } from "../../components/section/SectionComponent";
 
 const sections = new SectionComponent();
-sections.setAttribute("data-url", "src/data/sections.json");
+sections.setAttribute("data-url", "public/data/sections.json");
 export const homeView = async () => `
-  <section-component data-url="src/data/sections.json"></section-component>
+  <section-component data-url="public/data/sections.json"></section-component>
 `;
